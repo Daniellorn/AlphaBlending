4d6eda78229ac1c722dc6287b6c6434c3a584dfc
Daniellorn <nadolny.daniel15@gmail.com>
2024-12-21 13:54:26 +0100

    Implementacja funkcji mieszajacych

--- blender.cpp
+++ blender.cpp
@@ -1,3 +1,118 @@
 #include "blender.h"
 
+#include <QDebug>
+
 Blender::Blender() {}
+
+void Blender::blend(QImage &background, const QImage &foreground, float alpha, int mode)
+{
+    if (background.size() != foreground.size())
+    {
+        qDebug() << "Obrazki roznej wielkosci";
+        std::exit(-1);
+    }
+
+    int ymax = background.height();
+    int xmax = background.width();
+
+    for(int y = 0; y < ymax; y++)
+    {
+        for (int x = 0; x < xmax; x++)
+        {
+            PixelColor pixelB = getPixel(background, x, y);
+            PixelColor pixelF = getPixel(foreground, x, y);
+
+
+
+
+        }
+    }
+}
+
+PixelColor Blender::normal(PixelColor &backgroundPixel, PixelColor &foregroundPixel)
+{
+    return foregroundPixel;
+}
+
+PixelColor Blender::multiply(PixelColor &backgroundPixel, PixelColor &foregroundPixel)
+{
+    return backgroundPixel * foregroundPixel;
+}
+
+PixelColor Blender::screen(PixelColor& backgroundPixel, PixelColor& foregroundPixel)
+{
+    return backgroundPixel + foregroundPixel - (backgroundPixel * foregroundPixel);
+}
+
+PixelColor Blender::overlay(PixelColor& backgroundPixel, PixelColor& foregroundPixel)
+{
+    PixelColor result;
+
+    result.R = (backgroundPixel.R <= 128)
+                   ? multiply(backgroundPixel, foregroundPixel).R
+                   : screen(backgroundPixel, foregroundPixel).R;
+
+    result.G = (backgroundPixel.G <= 128)
+                   ? multiply(backgroundPixel, foregroundPixel).G
+                   : screen(backgroundPixel, foregroundPixel).G;
+
+    result.B = (backgroundPixel.B <= 128)
+                   ? multiply(backgroundPixel, foregroundPixel).B
+                   : screen(backgroundPixel, foregroundPixel).B;
+
+    return result;
+
+}
+
+PixelColor Blender::darken(PixelColor &backgroundPixel, PixelColor &foregroundPixel)
+{
+    int BrightnessBackground = (backgroundPixel.R + backgroundPixel.G + backgroundPixel.B) / 3;
+    int BrightnessForeground = (foregroundPixel.R + foregroundPixel.G + foregroundPixel.B) / 3;
+
+    return (BrightnessBackground < BrightnessForeground) ? backgroundPixel: foregroundPixel;
+}
+
+PixelColor Blender::lighten(PixelColor &backgroundPixel, PixelColor &foregroundPixel)
+{
+    int BrightnessBackground = (backgroundPixel.R + backgroundPixel.G + backgroundPixel.B) / 3;
+    int BrightnessForeground = (foregroundPixel.R + foregroundPixel.G + foregroundPixel.B) / 3;
+
+    return (BrightnessBackground < BrightnessForeground) ? foregroundPixel: backgroundPixel;
+}
+
+PixelColor Blender::difference(PixelColor &backgroundPixel, PixelColor &foregroundPixel)
+{
+    return backgroundPixel - foregroundPixel;
+}
+
+PixelColor Blender::additive(PixelColor &backgroundPixel, PixelColor &foregroundPixel)
+{
+    return backgroundPixel + foregroundPixel;
+}
+
+PixelColor Blender::getPixel(const QImage &img, int x, int y)
+{    
+    const uchar* line = img.scanLine(y);
+    uint8_t blue = line[4*x]; //blue
+    uint8_t green = line[4*x + 1]; //green
+    uint8_t red = line[4*x + 2]; //red
+    //uint8_t alpha = line[4*x + 3]; // alpha
+
+
+    return PixelColor{red, green, blue};
+
+}
+
+void Blender::setPixel(QImage &img, int x, int y, const PixelColor& color)
+{
+    int red = color.R;
+    int green = color.G;
+    int blue = color.B;
+
+    uchar* line = img.scanLine(y);
+    line[4*x] = blue; //blue
+    line[4*x + 1] = green; //green
+    line[4*x + 2] = red; //red
+    //line[4*x + 3] = 255; // alpha
+
+}
--- blender.h
+++ blender.h
@@ -3,6 +3,52 @@
 
 #include <QImage>
 
+struct PixelColor
+{
+    // tu pewnie uint8 powinien byc
+    int R;
+    int G;
+    int B;
+
+    PixelColor operator*(const PixelColor& other) const
+    {
+        return PixelColor{
+            (R * other.R) >> 8,
+            (G * other.G) >> 8,
+            (B * other.B) >> 8
+        };
+    }
+
+    PixelColor operator*(int value) const
+    {
+        return PixelColor{
+            (R * value) / 255,
+            (G * value) / 255,
+            (B * value) / 255
+        };
+    }
+
+
+    PixelColor operator+(const PixelColor& other) const
+    {
+        return PixelColor{
+            std::min(255, (R + other.R)),
+            std::min(255, (G + other.G)),
+            std::min(255, (B + other.B))
+        };
+    }
+
+    PixelColor operator-(const PixelColor& other) const
+    {
+        return PixelColor{
+            std::max(0, (R - other.R)),
+            std::max(0, (G - other.G)),
+            std::max(0, (B - other.B))
+        };
+    }
+};
+
+
 class Blender
 {
 public:
@@ -12,7 +58,18 @@ public:
 
 private:
 
+    PixelColor normal(PixelColor& backgroundPixel,PixelColor& foregroundPixel);
+    PixelColor multiply(PixelColor& backgroundPixel,PixelColor& foregroundPixel);
+    PixelColor screen(PixelColor& backgroundPixel,PixelColor& foregroundPixel);
+    PixelColor overlay(PixelColor& backgroundPixel,PixelColor& foregroundPixel);
+    PixelColor darken(PixelColor& backgroundPixel,PixelColor& foregroundPixel);
+    PixelColor lighten(PixelColor& backgroundPixel,PixelColor& foregroundPixel);
+    PixelColor difference(PixelColor& backgroundPixel,PixelColor& foregroundPixel);
+    PixelColor additive(PixelColor& backgroundPixel,PixelColor& foregroundPixel);
 
+
+    PixelColor getPixel(const QImage& img, int x, int y);
+    void setPixel(QImage& img, int x, int y, const PixelColor& color);
     // Tu funkcje blendujace z moze jakas mapa<int, std::function>
 
 };
